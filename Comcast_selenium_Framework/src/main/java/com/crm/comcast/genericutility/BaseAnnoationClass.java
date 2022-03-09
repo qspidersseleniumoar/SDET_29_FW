49ef291c4f45c2628085b7e5210ae8f074f3c848
Deepak <Deepak@TYSS-Deepak>
2022-03-09 17:04:22 +0530

    push

--- Comcast_selenium_Framework/src/main/java/com/crm/comcast/genericutility/BaseAnnoationClass.java
+++ Comcast_selenium_Framework/src/main/java/com/crm/comcast/genericutility/BaseAnnoationClass.java
@@ -1,5 +1,7 @@
 package com.crm.comcast.genericutility;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -48,6 +50,7 @@ public class BaseAnnoationClass {
 	     }else if(BROWSER.equals("ie")) {
 	    	  driver = new InternetExplorerDriver();
 	     }
+	     driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	     driver.get(URL);
 	     sDriver = driver;
 	}
